e06a486532b2daa69c5bebdc130fa2ed6566101c
Khushi <kagarwal170106@gmail.com>
2026-05-04 00:00:13 +0530

    lc 53

---
+++ lc53.py
@@ -0,0 +1,14 @@
+# # Accepted
+# 210 / 210 testcases passed
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+
+        current_sum = nums[0]
+        max_sum = nums[0]
+        for num in nums[1:]:
+            current_sum = max(num, current_sum + num)
+            max_sum = max(max_sum, current_sum)
+
+    
+        return max_sum
